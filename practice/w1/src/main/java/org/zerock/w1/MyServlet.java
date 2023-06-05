f81c97677bb29b01ae340f30b0fc13c3d469591b
Erica-Ku <yeskuminji@naver.com>
2023-06-05 10:13:03 +0900

    연습 프로젝트 생성

--- practice/w1/src/main/java/org/zerock/w1/MyServlet.java
+++ practice/w1/src/main/java/org/zerock/w1/MyServlet.java
@@ -1,18 +1,22 @@
 package org.zerock.w1;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet
+@WebServlet(name = "myServlet", urlPatterns = "/my")
 public class MyServlet extends HttpServlet {
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
 
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>MyServlet</h1>");
+        out.println("</body></html>");
     }
 }
