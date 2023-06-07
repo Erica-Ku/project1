8c5bbc607255953c273e10ad2242fa03dd2d99e1
Erica-Ku <yeskuminji@naver.com>
2023-06-07 15:01:34 +0900

    w2 프로젝트 생성

---
+++ practice/w2/src/main/java/org/zerock/w2/controller/LoginController.java
@@ -0,0 +1,20 @@
+package org.zerock.w2.controller;
+
+import lombok.extern.java.Log;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/login")
+@Log
+public class LoginController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.info("login get.............");
+        req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req, resp);
+    }
+}
