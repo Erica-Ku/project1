67814383a2d1eba6ee40bd5568a27ccb6a72199f
Erica-Ku <yeskuminji@naver.com>
2023-07-03 13:01:35 +0900

    다대일 연관관계 실습

--- practice/b01/src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
+++ practice/b01/src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.zerock.b01.domain.Board;
+import org.zerock.b01.dto.BoardListReplyCountDTO;
 
 import java.util.List;
 import java.util.Optional;
@@ -84,7 +85,7 @@ public class BoardRepositoryTests {
         Page<Board> result = boardRepository.searchAll(types, keyword, pageable);
     }
 
-    @Test
+//    @Test
     public void testSearchAll2() {
         String[] types = {"t", "c", "w"};
         String keyword = "1";
@@ -94,6 +95,19 @@ public class BoardRepositoryTests {
         log.info(result.getSize());
         log.info(result.getNumber());
         log.info(result.hasPrevious() + ": " + result.hasNext());
+        result.getContent().forEach(board -> log.info(board));
+    }
+
+    @Test
+    public void testSearchReplyCount() {
+        String[] types = {"t", "c", "w"};
+        String keyword = "1";
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+        Page<BoardListReplyCountDTO> result = boardRepository.searchWithReplyCount(types, keyword, pageable);
+        log.info(result.getTotalPages());
+        log.info(result.getSize());
+        log.info(result.getNumber());
+        log.info(result.hasPrevious() +": " + result.hasNext());
         result.getContent().forEach(board -> log.info(board));
     }
 }
