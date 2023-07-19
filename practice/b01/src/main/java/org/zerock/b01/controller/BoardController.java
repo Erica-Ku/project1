824872ebc7dcb8a50e9edd075bf5942e04cd73da
Erica-Ku <yeskuminji@naver.com>
2023-07-19 15:48:17 +0900

    스프링 시큐리티 적용하기

--- practice/b01/src/main/java/org/zerock/b01/controller/BoardController.java
+++ practice/b01/src/main/java/org/zerock/b01/controller/BoardController.java
@@ -6,6 +6,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -74,6 +75,7 @@ public class BoardController {
     }
 
 
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/register")
     public void registerGET(){
 
@@ -111,7 +113,7 @@ public class BoardController {
 //
 //    }
 
-
+    @PreAuthorize("isAuthenticated()")
     @GetMapping({"/read", "/modify"})
     public void read(Long bno, PageRequestDTO pageRequestDTO, Model model){
 
@@ -123,6 +125,7 @@ public class BoardController {
 
     }
 
+    @PreAuthorize("principal.username == #boardDTO.writer")
     @PostMapping("/modify")
     public String modify( @Valid BoardDTO boardDTO,
                           BindingResult bindingResult,
@@ -166,7 +169,7 @@ public class BoardController {
 //
 //    }
 
-
+    @PreAuthorize("principal.username == #boardDTO.writer")
     @PostMapping("/remove")
     public String remove(BoardDTO boardDTO, RedirectAttributes redirectAttributes) {
 
